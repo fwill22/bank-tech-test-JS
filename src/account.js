9956a613b7fbe0e2e9f3a521b33f8450b4e46e15
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-08-16 12:37:00 +0100

    Refactored deposit and withdrawal methods - separated validations into private methods for each

--- src/account.js
+++ src/account.js
@@ -8,16 +8,30 @@ class Account {
   }
 
   deposit = (amount) => {
+    this._depositValidations(amount)
+    return this.transactionHistory.push(new Transaction(amount, amount + this.balance()))
+  }
+
+  withdraw = (amount) => {
+    this._withdrawalValidations(amount)
+    let withdrawal = - amount
+    return this.transactionHistory.push(new Transaction(- amount, this.balance() - amount))
+  }
+
+  balance = () => {
+    return this.transactionHistory.map(transaction => transaction.amount).reduce((a,b) => a + b, 0)
+  }
+
+  _depositValidations = (amount) => {
     if (isNaN(amount) === true) {
       throw new Error ('Invalid Data Type: Please enter an integer')
     }
     else if (amount < 0) {
       throw new Error ('Invalid amount: unable to deposit negative sum')
     }
-    return this.transactionHistory.push(new Transaction(amount, amount + this.balance()))
   }
 
-  withdraw = (amount) => {
+  _withdrawalValidations = (amount) => {
     if (isNaN(amount) === true) {
       throw new Error ('Invalid Data Type: Please enter an integer')
     }
@@ -27,12 +41,6 @@ class Account {
     else if (amount > this.balance() + this.overdraftLimit) {
       throw new Error ('Insufficient Funds')
     }
-    let withdrawal = - amount
-    return this.transactionHistory.push(new Transaction(- amount, this.balance() - amount))
-  }
-
-  balance = () => {
-    return this.transactionHistory.map(transaction => transaction.amount).reduce((a,b) => a + b, 0)
   }
 }
 
