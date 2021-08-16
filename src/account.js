9420bffe9b5e3f167589bd52c2d343ce6de98644
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-08-16 11:14:38 +0100

    Deposit and balance methods added - transactionHistory array included in constructor. All tests passing

--- src/account.js
+++ src/account.js
@@ -1,6 +1,15 @@
 class Account {
   constructor(openingBalance = 0) {
     this.openingBalance = openingBalance;
+    this.transactionHistory = []
+  }
+
+  deposit = (amount) => {
+    return this.transactionHistory.push(amount)
+  }
+
+  balance = () => {
+    return this.transactionHistory.reduce((a,b) => a + b, 0)
   }
 }
 
--- test/account.test.js
+++ test/account.test.js
@@ -1,6 +1,20 @@
+/* eslint-disable no-undef */
 const Account = require('../src/account');
 
+beforeEach(function() {
+  let account = new Account()
+
 test( 'opening balance is zero', () => {
-  const account = new Account()
   expect(account.openingBalance).toBe(0);
 }) 
+
+test( 'deposit can be saved to transaction history record', () => {
+  account.deposit(100)
+  expect(account.transactionHistory).toContain(100)
+})
+
+test( 'can deposit money to account', () => {
+  account.deposit(100)
+  expect(account.balance()).toEqual(100)
+})
+})
