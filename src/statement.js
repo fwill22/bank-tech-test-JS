4ab47a796576204671c828106d0aa799f5d1d119
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-08-16 12:57:13 +0100

    Pseudocode for Statement class methods. Placeholder

--- src/statement.js
+++ src/statement.js
@@ -1,8 +1,36 @@
 class Statement {
   
+  // PSEUDOCODE:
+
+  formatAmount = (transaction) => {
+    if (transaction.amount === 0)
+    // print BLANK SPACE
+    else if (transaction.amount < 0) {
+
+    // remove minus symbol and format with .toFixed(2)
+    // ||  || transaction amount without - .toFixed(2) ||
+  } else {
+  // format with .toFixed(2)
+  //}
+}
+
+  formatBalance = (transaction) => {
+    add decimals and || ||
+  }
+
+  formatStatement (transactionHistory) => {
+    map.reverse through transactionHistory and apply below formatting: 
+    "`${Date}` +  this.formatAmount(transaction) + this.formatBalance(transaction) + \n"
+  }
+
   header = () => {
     return "|    date    ||  credit  ||  debit  || balance  |"
   }
+
+  printStatement = (transactionHistory) => {
+    console.log(header + formatStatement(transactionHistory))
+  }
+  
 }
 
 module.exports = Statement;
--- test/statement.test.js
+++ test/statement.test.js
@@ -4,15 +4,23 @@ const Statement = require('../src/statement');
 describe( 'Statement', () => {
   statement = new Statement
 
-  test( 'has a header', () => {
-    expect(statement.header()).toEqual("|    date    ||  credit  ||  debit  || balance  |")
+  test( 'it can print a statement', () => {
+    expect(statement.print_(transactionHistory)).toEqual( `|    date    ||  credit  ||  debit  || balance  |\n`
+                                                          `| ${new Date().toLocaleDateString()} ||  || 500.00 || 2500.00 |\n`
+                                                          `| ${new Date().toLocaleDateString()} || 2000.00 ||  || 3000.00 |\n`
+                                                          `| ${new Date().toLocaleDateString()} || 1000.00 ||  || 1000.00 |\n`)
   })
  
 })
 
 
 // METHODS:
-// format(amount)
-// format(transaction)
-// format(statement)
-// print_(transaction_history)
+// format(amount) - if 0 nothing printed, print to 2 decimal places. 
+// format(transaction) - if minus value, print in debit column, if + value, print in credit column
+// format(statement) - order transactionHistory in reverse chronology
+// print_(transaction_history) - console log statement. 
+
+// let(:credit_transaction) { double(:transaction, date: Date.today, credit: 1000, debit: 0, updated_balance: 1000) }
+// let(:credit_transaction2) { double(:transaction, date: Date.today, credit: 2000, debit: 0, updated_balance: 3000) }
+// let(:debit_transaction) { double(:transaction, date: Date.today, credit: 0, debit: 500, updated_balance: 2500) }
+// let(:transaction_history) { [credit_transaction, credit_transaction2, debit_transaction] } 
